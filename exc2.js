c01713c51d992ab159b197a39ae3223ef134e6b5
Thaina Araújo <105496462+1337thai@users.noreply.github.com>
2022-06-08 21:48:12 -0300

    Update exc2.js

---
+++ exc2.js
@@ -0,0 +1,43 @@
+var mes = parseInt(prompt("Digite o número correspondente ao mês:"))
+
+switch(mes){
+   
+  case 1:
+    console.log("O mês corresponde a Janeiro")
+   break 
+  case 2:
+   console.log("O mês corresponde a Fevereiro")
+  break
+  case 3:
+   console.log("O mês corresponde a Março") 
+  break
+  case 4:
+   console.log("O mês corresponde a Abril")
+  break
+   case 5:
+   console.log("O mês corresponde a Maio")
+  break
+   case 6:
+   console.log("O mês corresponde a Junho")
+  break
+  case 7:
+   console.log("O mês corresponde a Julho")
+  break
+ case 8:
+   console.log("O mês corresponde a Agosto")
+  break
+  case 9:
+   console.log("O mês corresponde a Setembro")
+  break
+  case 10:
+   console.log("O mês corresponde a Outubro")
+  break
+  case 11:
+   console.log("O mês corresponde a Novembro")
+  break
+  case 12:
+   console.log("O mês corresponde a Dezembro")
+  break
+  default:
+    console.log("Código inválido")
+}
