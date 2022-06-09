719741145b88c1793cc39bc00ba31952c6b72f2c
Thaina Araújo <105496462+1337thai@users.noreply.github.com>
2022-06-08 21:42:57 -0300

    Initial commit

---
+++ exc1.js
@@ -0,0 +1,4 @@
+var dist = prompt ("Digite a distância total percorrida:")
+var combust = prompt ("Digite o total de combustível gasto:")
+var resul = (dist/combust)
+ console.log("O consumo médio do automóvel é de "resul.toFixed(3))
